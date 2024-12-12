0981fd581b1e8b88dae7793df05087b7ef8d1b3e
tungduong0708 <tungduong0708@gmail.com>
2024-12-12 18:24:47 +0700

    complete merge

--- src/game_state.cpp
+++ src/game_state.cpp
@@ -252,9 +252,6 @@ void MapBuilderState::draw() {
     DrawButton(pauseButton, *game);
 }
 
-Player player("Player", 0, 0, true, false, 0, 0, 0, 0, Vector2{0, 0}, 0, 0, {}, "mario");
-MyCamera camera;
-
 GameplayState::GameplayState(Game* game)
     : GameState(game)
 {
@@ -267,16 +264,9 @@ GameplayState::GameplayState(Game* game)
     TextHelper::loadFont("highway_gothic", "");
     TextHelper::loadTexture("coin");
 
-
-    ImageSet idleImageSet = IDLE;
-    movingObjects.push_back(player.copy());
-
-    movingObjects[0]->Init(b2Vec2(10.0f, 12.0f), idleImageSet);
-
     TilesetHandler::Init();
     Tilemap* tilemap = Tilemap::getInstance();
     tilemap->LoadMapFromJson("resources/tilemaps/map-1-1.json");
-    camera = MyCamera(38.0f, Vector2{ (float)tilemap->GetWidth(), (float)tilemap->GetHeight() }, game->getScreenWidth(), game->getScreenHeight());
     
     float buttonWidth = 35;
     float buttonHeight = 35;
@@ -287,23 +277,9 @@ GameplayState::GameplayState(Game* game)
 void GameplayState::update() {
     float deltaTime = GetFrameTime();
     Physics::Update(deltaTime); 
-    if (GetMouseWheelMove() > 0) camera.SetZoom(camera.GetZoom() * 1.1f);
-    if (GetMouseWheelMove() < 0) camera.SetZoom(camera.GetZoom() / 1.1f);
 
-
-    for (int i = 0; i < movingObjects.size(); i++) {
-        Vector2 pos = movingObjects[i]->getPosition();
-        b2Vec2 vel = movingObjects[i]->getVelocity();
-        movingObjects[i]->HandleInput();
-        movingObjects[i]->Update(Vector2{vel.x, vel.y}, deltaTime);
-    }
-
-    b2Vec2 velocity = movingObjects[0]->getVelocity();
-    Vector2 position = movingObjects[0]->getPosition();
-    //cout << "player position: " << position.x << " " << position.y << endl;
-    camera.Update(movingObjects[0]->getPosition());  
     Tilemap* tilemap = Tilemap::getInstance();
-    tilemap->Update(Vector2{velocity.x, velocity.y}, deltaTime);
+    tilemap->Update(deltaTime);
 
     // Update pause button hover state
     pauseButton.isHovered = CheckCollisionPointRec(GetMousePosition(), pauseButton.rect);
@@ -315,29 +291,14 @@ void GameplayState::update() {
 }
 
 void GameplayState::draw() {
-    BeginMode2D(camera.GetCamera());
-
     Tilemap* tilemap = Tilemap::getInstance();
     tilemap->Draw();
-    for (int i = 0; i < movingObjects.size(); i++) {
-        movingObjects[i]->Draw();
-    }
 
-    Vector2 cameraTarget = camera.GetCameraTarget();
-    movingObjects[0]->Draw(Vector2{cameraTarget.x - 9.0f, cameraTarget.y - 7.0f}, 0.0f);
-    
-    Physics::world.DebugDraw(); 
-
-    EndMode2D();
     DrawButton(pauseButton, *game);
 }
 
 void GameplayState::cleanup() {
     TilesetHandler::Clear();
-
-    for (int i = 0; i < movingObjects.size(); i++) {
-        delete movingObjects[i];
-    } 
 }
 
 PauseGameState::PauseGameState(Game* game)
