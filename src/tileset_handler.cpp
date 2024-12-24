600b1e94770d9c50ed6518cc708a8dce8d144cda
Tram <tram14042005@gmail.com>
2024-12-24 14:19:34 +0700

    update map2

--- src/tileset_handler.cpp
+++ src/tileset_handler.cpp
@@ -30,7 +30,8 @@ void TilesetHandler::Init()
                                               "resources/tilesets/bg-3-2",
                                               "resources/tilesets/bg-3-3",
                                               "resources/tilesets/bg-3-4",
-                                              "resources/tilesets/Re_draw"};
+                                              "resources/tilesets/Re_draw",
+                                              "resources/tilesets/ghosthouse"};
     for (const auto& tilesetPath : tilesetPaths) {
         addTileSet(tilesetPath + ".json", tilesetPath + ".png");
     }
