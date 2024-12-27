a221bdfbbb3c7acf31be0a2343afa0e3bd0f5e46
ltdsword <ltdsp1514@gmail.com>
2024-12-28 03:40:43 +0700

    buff luigi

--- src/player.cpp
+++ src/player.cpp
@@ -36,14 +36,14 @@ Player::Player(string type, string name, float coins, int lives, int health,
     if (type == "mario") {
         this->time = 300.0f;
         this->speed = 8.5f;
-        force = -26.0f;
+        force = -27.0f;
         bulletSpeed = 9.0f;
         bulletFreq = 0.37f;
     }
     else {
         this->time = 300.0f;
-        this->speed = 7.2f;
-        force = -28.5f;
+        this->speed = 7.5f;
+        force = -29.0f;
         bulletSpeed = 8.0f;
         bulletFreq = 0.30f;
     }
